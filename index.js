31fad9509989b85fc2518ba9c832632c2def6286
Christian Bundy <christianbundy@fraction.io>
2018-12-21 11:06:04 -0800

    Avoid changine `since` on initialization

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const pullCursor = require('pull-cursor')
 
-module.exports = (array, since) => {
-  since.set(array.length - 1)
+const getMeta = (array) => (offset, useCache, cb) => {
+  if (offset < 0 || offset >= array.length) {
+    return cb(new Error('out of bounds:' + offset))
+  }
 
-  return pullCursor(
-    since,
-    (offset, useCache, cb) => {
-      if (offset < 0 || offset >= array.length) {
-        return cb(new Error('out of bounds:' + offset))
-      }
-
-      cb(null, array[offset], offset - 1, offset + 1)
-    })
+  cb(null, array[offset], offset - 1, offset + 1)
 }
+
+module.exports = (array, since) => 
+  pullCursor(since, getMeta(array))
